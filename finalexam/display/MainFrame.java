e2e4d5527b9b58f2a4a1d75a8ffc585847eb5772
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-11-27 22:08:25 +0700

    Delete finalexam directory

--- finalexam/display/MainFrame.java
+++
@@ -1,8 +0,0 @@
-package finalexam.display;
-
-public class MainFrame {
-    //Nếu như mà là chế độ đơn -> Set width, length của Display là x1, y1
-
-    //Nếu như mà là chế độ so sánh -> Set width, length của Display là x2, y2
-    // Chia 2 màn hình
-}
